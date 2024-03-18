38098a4a8e48969ce7c5cdbbc3b65ac36e304765
tkm-mkzk <tkm.mkzk.0604@gmail.com>
2024-03-18 19:07:41 +0900

    todoモデル作成

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,15 @@
-console.log('hello express')
+import express from 'express'
+import type { Express, Request, Response } from 'express'
+import { PrismaClient } from '@prisma/client'
+
+const app: Express = express()
+const PORT = 8080
+
+const prisma = new PrismaClient()
+
+app.get('/allTodos', async (req: Request, res: Response) => {
+  const allTodos = await prisma.todo.findMany()
+  return res.json(allTodos)
+})
+
+app.listen(PORT, () => console.log('server is running🚀'))
